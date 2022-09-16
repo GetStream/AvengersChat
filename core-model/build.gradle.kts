790d2f26ce4758138613edb968b091f9635c5347
skydoves <skydoves@naver.com>
2022-09-16 13:49:10 +0900

    implement core-model module

---
+++ core-model/build.gradle.kts
@@ -0,0 +1,30 @@
+import io.getstream.Configuration
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id(libs.plugins.android.library.get().pluginId)
+    id(libs.plugins.kotlin.android.get().pluginId)
+    id(libs.plugins.kotlin.parcelize.get().pluginId)
+    id(libs.plugins.ksp.get().pluginId) version libs.versions.ksp.get()
+}
+
+android {
+    compileSdk = Configuration.compileSdk
+
+    defaultConfig {
+        minSdk = Configuration.minSdk
+        targetSdk = Configuration.targetSdk
+    }
+}
+
+dependencies {
+    // stream client
+    implementation(libs.stream.client)
+
+    // json parsing
+    implementation(libs.moshi)
+    ksp(libs.moshi.codegen)
+
+    // logger
+    api(libs.timber)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "AvengersChat"
 include(":app")
+include(":core-model")
